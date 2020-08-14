f812d58b05043d663aa6df046a44e3e87bcede00
Lorenzo Castellino <castellino96@gmail.com>
2020-08-15 00:36:05 +0200

    Add function to find gas station in defined circle

--- gasapi/gasapi/crud.py
+++ gasapi/gasapi/crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
-from math import cos
+from sqlalchemy import and_
+from math import cos, radians, degrees, asin
 from . import models, schemas
 
 def get_place(db: Session, place_id: int):
@@ -8,6 +9,14 @@ def get_place(db: Session, place_id: int):
 def get_places(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Place).filter(models.Place.prices.any()).order_by(models.Place.id.asc()).offset(skip).limit(limit).all()
 
+def get_in_radius(db: Session, lat: float, long: float, radius: int = 15):
+    R = 6371 # radius of heart in km
+    maxlat = lat + degrees(radius/R)
+    minlat = lat - degrees(radius/R)
+    maxlong = long + degrees(asin(radius/R) / cos(radians(lat)))
+    minlong = long - degrees(asin(radius/R) / cos(radians(lat)))
+
+    return db.query(models.Place).filter(and_(models.Place.lat.between(minlat, maxlat), models.Place.long.between(minlong, maxlong))).all()
 
 
     
